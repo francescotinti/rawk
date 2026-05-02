d4700c9ee54c34fef2ab1b41bd922be4cb6b3dd9
Francesco Tinti <francesco.tinti@activemind.it>
2026-05-03 01:12:47 +0200

    Expand integration tests for strings, regex, flow control, and coercion

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -56,3 +56,39 @@ fn test_bitwise_and_time() {
         .success()
         .stdout(predicate::str::contains("1 4"));
 }
+
+#[test]
+fn test_regex_matching() {
+    let mut cmd = Command::cargo_bin("rawk").unwrap();
+    cmd.arg("BEGIN { match(\"hello 123 world\", \"[0-9]+\"); print RSTART, RLENGTH }")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("7 3"));
+}
+
+#[test]
+fn test_string_manipulation() {
+    let mut cmd = Command::cargo_bin("rawk").unwrap();
+    cmd.arg("BEGIN { s = \"foo bar\"; gsub(\"bar\", \"baz\", s); print s, length(s), toupper(s) }")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("foo baz 7 FOO BAZ"));
+}
+
+#[test]
+fn test_control_flow() {
+    let mut cmd = Command::cargo_bin("rawk").unwrap();
+    cmd.arg("BEGIN { sum = 0; i = 1; while(i<=5) { sum = sum + i; i = i + 1 }; print sum }")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("15"));
+}
+
+#[test]
+fn test_type_coercion() {
+    let mut cmd = Command::cargo_bin("rawk").unwrap();
+    cmd.arg("BEGIN { print \"10\" + 5, \"42\" == 42, \"abc\" + 0 }")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("15 1 0"));
+}
